0290ba805582902d99e36aab080b83b48192e31a
kaminion <jwsoft0529@gmail.ocm>
2023-05-01 21:15:03 +0900

    feature: add SAINT

---
+++ models/saint.py
@@ -0,0 +1,55 @@
+import numpy as np
+import torch 
+
+from torch.nn import Module, Parameter, Embedding, Linear, Transformer
+from torch.nn.init import normal_
+
+class SAINT(Module):
+    def __init__(
+            self, num_q, n, d, num_attn_heads, dropout, num_tr_layers=1
+    ):
+        super(SAINT, self).__init__()
+        self.num_q = num_q
+        self.n = n
+        self.d = d
+        self.num_attn_heads = num_attn_heads
+        self.dropout = dropout
+        self.num_tr_layers = num_tr_layers
+
+        self.E = Embedding(self.num_q, self.d)
+        self.R = Embedding(2, self.d)
+        self.P = Parameter(torch.Tensor(self.n, self.d))
+        self.S = Parameter(torch.Tensor(1, self.d))
+
+        normal_(self.P)
+        normal_(self.S)
+
+        self.transformer = Transformer(
+            self.d,
+            self.num_attn_heads,
+            num_encoder_layers=self.num_tr_layers,
+            num_decoder_layers=self.num_tr_layers,
+            dropout=self.dropout
+        )
+
+        self.pred = Linear(self.d, 1)
+
+    def forward(self, q, r):
+        batch_size = r.shape[0]
+
+        E = self.E(q).permute(1, 0, 2)
+        R = self.R(r[:, :-1]).permute(1, 0, 2)
+        S = self.S.repeat(batch_size, 1).unsqueeze(0)
+        R = torch.cat([S, R], dim=0)
+
+        P = self.P.unsqueeze(1)
+
+        mask = self.transformer.generate_square_subsequent_mask(self.n)
+        R = self.transformer(
+            E + P, R + P, mask, mask, mask
+        )
+        R = R.permute(1, 0, 2)
+
+        p = torch.sigmoid(self.pred(R)).squeeze()
+
+        return p
