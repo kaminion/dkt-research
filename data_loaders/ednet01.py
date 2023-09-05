b3caa381151781ad92e5f7443a90325d542f49fe
kaminion <anysweetimes@naver.com>
2023-09-05 10:43:42 +0900

    refactor: udf업데

--- data_loaders/ednet01.py
+++ data_loaders/ednet01.py
@@ -98,7 +98,7 @@ class EdNet01(Dataset):
                 print('start ========= ', qid)
                 print(u_df.loc[u_df['question_id'] == qid, 'question_id']," === : === ", qid)
                 # 문항번호와 유저번호가 같으면서 정답값도 같다면 1값 할당
-                u_df.loc[(['question_id'] == qid) & (q_df.loc['question_id'] == qid) & (q_df['correct_answer'] == u_df['user_answer']), 'correct'] = 1
+                u_df.loc[(u_df['question_id'] == qid) & (q_df.loc['question_id'] == qid) & (q_df['correct_answer'] == u_df['user_answer']), 'correct'] = 1
                 print('end ============= ', u_df)
             # 유저 아이디 시퀀스에 넣기
             u_seqs.append(np.array([u_id]))
