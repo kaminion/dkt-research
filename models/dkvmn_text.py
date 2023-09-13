ecbdc0a91092840e7909431503d3ca745fb83b62
kaminion <anysweetimes@naver.com>
2023-09-13 23:32:29 +0900

    refactor: rollback

--- models/dkvmn_text.py
+++ models/dkvmn_text.py
@@ -88,7 +88,7 @@ class SUBJ_DKVMN(Module):
                 p: the knowledge level about q
                 Mv: the value matrices from q, r, at
         '''
-        x = self.qr_emb_layer(q + q2diff * self.num_q).permute(0, 2, 1)
+        x = self.qr_emb_layer(q + r * self.num_q).permute(0, 2, 1)
         batch_size = x.shape[0]
 
         # BERT를 사용하지 않는다면 주석처리
