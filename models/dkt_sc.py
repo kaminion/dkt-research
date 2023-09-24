5c100a90d1b1cc9b8391af60ac6cc68fd8418d6f
kaminion <anysweetimes@naver.com>
2023-09-25 02:34:16 +0900

    refactor: len50 50

--- models/dkt_sc.py
+++ models/dkt_sc.py
@@ -110,7 +110,7 @@ class LSTMModel(Module):
             outs.append(hidden_l[0])
         
         out = outs[-1].squeeze() #.squeeze() 제외
-        print(":out:=====", outs[-1].shape)
+        print(":out:=====", outs[-1].shape, len(outs))
         out = self.fc(out)
         return out
 
