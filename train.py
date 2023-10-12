537313c78cda1ed98530fe1e0fb6260f364cad3b
kaminion <anysweetimes@naver.com>
2023-10-12 15:45:33 +0900

    refactor: train issue 해결

--- train.py
+++ train.py
@@ -175,6 +175,7 @@ def train_model(model, train_loader, valid_loader, num_q, num_epochs, opt, ckpt_
                 y = (y * one_hot(inpt_q, num_q)).sum(-1)
 
                 # y와 t 변수에 있는 행렬들에서 마스킹이 true로 된 값들만 불러옴
+                q = torch.masked_select(inpt_q, m).detach().cpu()
                 y = torch.masked_select(y, m).detach().cpu()
                 t = torch.masked_select(pred_t, m).detach().cpu()
                 h = torch.masked_select(hint_seqs, m).detach().cpu()
