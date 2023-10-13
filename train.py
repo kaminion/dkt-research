239c10e301f8810857d07cf001f244a2fe2ff5cc
kaminion <anysweetimes@naver.com>
2023-10-13 17:06:45 +0900

    refactor: DKVMN+를 위한 loss function 변경

--- train.py
+++ train.py
@@ -108,7 +108,7 @@ def train_model(model, train_loader, valid_loader, num_q, num_epochs, opt, ckpt_
 
             # 현재까지의 입력을 받은 뒤 다음 문제 예측
             y = model(q.long(), r.long(), bert_s, bert_t, bert_m) # sakt는 qshft_seqs.long() 추가
-            y = (y * one_hot(inpt_q, num_q)).sum(-1)
+            # y = (y * one_hot(inpt_q, num_q)).sum(-1)
 
             opt.zero_grad()
             y = torch.masked_select(y, m)
@@ -172,7 +172,7 @@ def train_model(model, train_loader, valid_loader, num_q, num_epochs, opt, ckpt_
 
                 
                 y = model(q.long(), r.long(), bert_s, bert_t, bert_m) # sakt는 qshft_seqs.long() 추가
-                y = (y * one_hot(inpt_q, num_q)).sum(-1)
+                # y = (y * one_hot(inpt_q, num_q)).sum(-1)
 
                 # y와 t 변수에 있는 행렬들에서 마스킹이 true로 된 값들만 불러옴
                 q = torch.masked_select(inpt_q, m).detach().cpu()
@@ -279,7 +279,7 @@ def test_model(model, test_loader, num_q, ckpt_path, mode=0, use_wandb=False):
                 
             model.eval()
             y = model(q.long(), r.long(), bert_s, bert_t, bert_m)
-            y = (y * one_hot(inpt_q, num_q)).sum(-1)
+            # y = (y * one_hot(inpt_q, num_q)).sum(-1)
 
             # y와 t 변수에 있는 행렬들에서 마스킹이 true로 된 값들만 불러옴
             q = torch.masked_select(inpt_q, m).detach().cpu()
