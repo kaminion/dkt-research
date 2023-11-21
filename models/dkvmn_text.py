bf4dc58c3477791a3af6c6a16e4310e16b01c4c9
kaminion <anysweetimes@naver.com>
2023-11-21 22:23:51 +0900

    refactor: pre-trained 모델 dimension 확인

--- models/dkvmn_text.py
+++ models/dkvmn_text.py
@@ -53,9 +53,9 @@ class SUBJ_DKVMN(Module):
 
         # BERT for feature extraction
         # bertconfig = BertConfig.from_pretrained('bert-base-uncased', output_hidden_states=True)
-        # self.bertmodel = BertModel.from_pretrained('bert-base-uncased', config=bertconfig)
         distilconfig = DistilBertConfig(output_hidden_states=True)
-        self.bertmodel = DistilBertModel(config=distilconfig)
+        self.bertmodel = BertModel.from_pretrained('bert-base-uncased', config=distilconfig)
+        # self.bertmodel = DistilBertModel(config=distilconfig)
         self.bertmodel.resize_token_embeddings(len(bert_tokenizer))
         # self.at_emb_layer = Sequential(
         #     Linear(768, self.dim_s),
